397f4cbf0d23217fae9c4c179df66a0344413caf
Jose Elias Barrientos <barrientosjoseelias@hotmail.com>
2021-12-04 23:34:58 -0400

    notificaciones push

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,5 @@
 import { Component } from '@angular/core';
 import { HttpClient, HttpErrorResponse, HttpHeaders } from '@angular/common/http';
-import { SwPush } from '@angular/service-worker';
 import { Options } from 'selenium-webdriver';
 import { environment } from '../environments/environment';
 import { UsersService } from './users/users.service';
@@ -12,72 +11,6 @@ import { UsersService } from './users/users.service';
 })
 export class AppComponent {
   title = 'projectService';
-  public readonly VAPID_PUBLIC_KEY = 'BNiSC8uZYtVccTg-pYMUk2WNFZ5UfPAmfYzEK7OpbJCkkT0-R5h-H0YPRKXKCI0_CwtB5y48T0LKaoiXIjVraow';
 
-  constructor(
- 	private _http: HttpClient,
-	private swPush: SwPush,
-  private service: UsersService
-  ){
-    this.subscribeToNotifications();
-  }
-
-  subscribeToNotifications(): any {
-
-    if(this.getCookie('endpoint') == '' || 
-       this.getCookie('p256dh') == '' || 
-       this.getCookie('auth') == ''){
-
-        this.swPush.requestSubscription({
-          serverPublicKey: this.VAPID_PUBLIC_KEY
-        }).then( sub => {
-          const token = JSON.parse(JSON.stringify(sub));
-          
-          let data = {
-            "token" : token
-          }
-            
-          this.service.postUrl('saveToken', data).then(response => {
-            this.setCookie('endpoint', token.endpoint, 365);
-            this.setCookie('p256dh', token.keys.p256dh, 365);
-            this.setCookie('auth', token.keys.auth, 365);
-            console.log(response);
-          }).catch(err => {
-            console.log(err)
-          });
-
-        }).catch(err => console.error('UPS :(', err));
-
-    }
-  }
-
-  public setCookie(cname, cvalue, exdays) {
-    const d = new Date();
-    d.setTime(d.getTime() + (exdays*24*60*60*1000));
-    let expires = "expires="+ d.toUTCString();
-
-    //Elimino la cookie anterior
-    let lastexpires = "expires=Thu, 01 Jan 1970 00:00:00 UTC";
-    document.cookie = cname + "=" + cvalue + ";" + lastexpires + ";path=/";
-
-    //Inserto la nueva cookie
-    document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
-  }
-
-  public getCookie(cname) {
-    let name = cname + "=";
-    let decodedCookie = decodeURIComponent(document.cookie);
-    let ca = decodedCookie.split(';');
-    for(let i = 0; i <ca.length; i++) {
-      let c = ca[i];
-      while (c.charAt(0) == ' ') {
-        c = c.substring(1);
-      }
-      if (c.indexOf(name) == 0) {
-        return c.substring(name.length, c.length);
-      }
-    }
-    return "";
-  }
-
+  constructor(private _http: HttpClient){}
 }
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -6,6 +6,8 @@ import Swal from 'sweetalert2';
 import { isNullOrUndefined } from 'util';
 import { Router } from '@angular/router';
 import $ from 'jquery';
+import { SwPush } from '@angular/service-worker';
+import { UsersService } from '../users/users.service';
 
 @Component({
   selector: 'app-home',
@@ -14,12 +16,17 @@ import $ from 'jquery';
 })
 export class HomeComponent implements OnInit {
 
+	public readonly VAPID_PUBLIC_KEY = 'BNiSC8uZYtVccTg-pYMUk2WNFZ5UfPAmfYzEK7OpbJCkkT0-R5h-H0YPRKXKCI0_CwtB5y48T0LKaoiXIjVraow';  
 	public httpOption : any;
 	public username : any;
 	public bLogger = false;
 	public role : any;
 
-	constructor(private router: Router) {}
+	constructor(private router: Router,
+				private swPush: SwPush,
+				private service: UsersService) {
+					this.subscribeToNotifications();
+				}
 
 	ngOnInit() {
 	    
@@ -35,6 +42,63 @@ export class HomeComponent implements OnInit {
 	    	this.router.navigate(['/'], { replaceUrl: true });
 	    }
 
+	}
+
+	subscribeToNotifications(): any {
+		if(this.getCookie('endpoint') == '' || 
+			this.getCookie('p256dh') == '' || 
+			this.getCookie('auth') == ''){
+
+			this.swPush.requestSubscription({
+				serverPublicKey: this.VAPID_PUBLIC_KEY
+			}).then( sub => {
+				const token = JSON.parse(JSON.stringify(sub));
+				
+				let data = {
+					"token" : token
+				}
+				
+				this.service.postUrl('saveToken', data).then(response => {
+					this.setCookie('endpoint', token.endpoint, 365);
+					this.setCookie('p256dh', token.keys.p256dh, 365);
+					this.setCookie('auth', token.keys.auth, 365);
+					console.log(response);
+				}).catch(err => {
+					console.log(err)
+				});
+
+			}).catch(err => console.error('UPS :(', err));
+
+		}
+	}
+	
+	public setCookie(cname, cvalue, exdays) {
+		const d = new Date();
+		d.setTime(d.getTime() + (exdays*24*60*60*1000));
+		let expires = "expires="+ d.toUTCString();
+
+		//Elimino la cookie anterior
+		let lastexpires = "expires=Thu, 01 Jan 1970 00:00:00 UTC";
+		document.cookie = cname + "=" + cvalue + ";" + lastexpires + ";path=/";
+
+		//Inserto la nueva cookie
+		document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+	}
+
+	public getCookie(cname) {
+		let name = cname + "=";
+		let decodedCookie = decodeURIComponent(document.cookie);
+		let ca = decodedCookie.split(';');
+		for(let i = 0; i <ca.length; i++) {
+			let c = ca[i];
+			while (c.charAt(0) == ' ') {
+				c = c.substring(1);
+			}
+			if (c.indexOf(name) == 0) {
+				return c.substring(name.length, c.length);
+			}
+		}
+		return "";
 	}
 
 	public logout(){
